c6ab3b1a54d2266686ee66b126658a76e8ff7cfe
Juliette <git@juliette.page>
2021-05-12 09:47:11 -0400

    Todo it

--- wand/ViewControllers/Home/SpellsTableViewController.swift
+++ wand/ViewControllers/Home/SpellsTableViewController.swift
@@ -10,10 +10,23 @@ import UIKit
 
 class SpellsTableViewController : UITableViewController {
     
+    var spells: [Spell]! = []
+    
     override func viewDidLoad () {
         super.viewDidLoad()
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .plain, target: self, action: #selector(addButtonAction(_:)))
+        
+        // load data
+        do {
+            spells = try loadSpells()
+        } catch {
+            let alert = UIAlertController(title: "Error!", message: "Failed to load data, \(error)", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            }))
+            self.present(alert, animated: true, completion: nil)
+        }
+        // TODO: load data onto table
     }
     
     @objc func addButtonAction (_ sender: UIButton) {
