eb945a03cf1e43d924aee11ba6fbbc7810ef213e
Juliette <git@juliette.page>
2021-05-15 14:00:05 -0400

    Working spells

--- wand/Structs and Enums/Spell.swift
+++ wand/Structs and Enums/Spell.swift
@@ -48,45 +48,106 @@ func loadSpells () throws -> [Spell] {
 func getAndRunSpell (_ points: [WPoint]) throws {
     // Loading spells
     let spells = try loadSpells()
-    let accuracy = 0
     var found: Spell?
+    let maxScore = 30
+    var same = false
     
+    // How spells are found:
+    // Loop over all the spells
+    // Loop over all points in the spell
+    // Compare points, adding a score until they're the same
+    // If the score is less than the max score, add the pog
+    // If it's not, remove the pog
+    
+    // 🍝
     for spell in spells {
-        // I love nested for loops
-        var i = 0
-        var score = 0
+        print("spell")
+        var index = 0
         for point in spell.points {
-            if i > points.count - 1 {
-                break
+            if index >= spell.points.count - 1 || index >= points.count - 1 {
+                continue
             }
-            if !comparePoint(pointA: point, pointB: points[i], accuracy: accuracy) {
-                break
+            
+            index = index + 1
+            
+            var i = 0
+            while i < maxScore {
+                // I don't think i've included enough loops tbh
+                print(i)
+                var doneX = false
+                var doneY = false
+                
+                if point.x == points[index].x + i {
+                    doneX = true
+                }
+                
+                if point.x == points[index].x - i && !doneX {
+                    doneX = true
+                }
+                
+                if point.y == points[index].y + i {
+                    doneY = true
+                }
+                
+                if point.y == points[index].y - i && !doneY {
+                    doneY = true
+                }
+                
+                if doneY && doneX {
+                    if doneY && doneX {
+                        same = true
+                    } else {
+                        same = false
+                    }
+                }
+                
+                i = i + 1
             }
-            i += 1
         }
-        if score == spell.points.count - 1 {
-            print("YEP1")
+        if same {
+            print("Found!!!")
             found = spell
+            break
         }
     }
+    
     
     print(found ?? "Not found")
-}
+    
+    if found == nil {
+        return
+    }
+    
+    // Sending request
+    let url = URL(string: "https://")!
+    var components = URLComponents(url: url, resolvingAgainstBaseURL: false)!
 
-func comparePoint (pointA: WPoint, pointB: WPoint, accuracy: Int) -> Bool {
-    var xSame = false
-    var ySame = false
-    for i in 0..<accuracy {
-        if pointA.x == pointB.x - i {
-            xSame = true
-        }
-    }
+    components.queryItems = [
+        URLQueryItem(name: "content", value: "Hello, spell")
+    ]
+
+    let query = components.url!.query
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    request.httpBody = Data(query!.utf8)
     
-    for i in 0..<accuracy {
-        if pointA.y == pointB.y - i {
-            ySame = true
+    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        guard let data = data,
+            let response = response as? HTTPURLResponse,
+            error == nil else {                                              // check for fundamental networking error
+            print("error", error ?? "Unknown error")
+            return
         }
+
+        guard (200 ... 299) ~= response.statusCode else {                    // check for http errors
+            print("statusCode should be 2xx, but is \(response.statusCode)")
+            print("response = \(response)")
+            return
+        }
+
+        let responseString = String(data: data, encoding: .utf8)
+        print("responseString = \(responseString)")
     }
-    
-    return xSame == ySame
+
+    task.resume()
 }
